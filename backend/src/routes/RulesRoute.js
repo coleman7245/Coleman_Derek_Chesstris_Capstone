76761e22dc2c5d979e2486e8a3a7ca810bfbb1df
coleman7245 <dlcoleman1@live.com>
2025-03-10 00:26:54 -0600

    Created the skeletons of the routes.

--- backend/src/routes/RulesRoute.js
+++ backend/src/routes/RulesRoute.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/about', async () => {
+router.get('/rules', async () => {
 
 });
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,14 +4,20 @@ import cors from 'cors';
 
 import connectDB from './db.js';
 
+import HomeRoute from './routes/HomeRoute.js';
+import GameRoute from './routes/GameRoute.js';
+import RankingRoute from './routes/RankingRoute.js';
+import RulesRoute from './routes/RulesRoute.js';
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', async (req, res) => {
-
-});
+app.use('/', HomeRoute);
+app.use('/game', GameRoute);
+app.use('/ranking', RankingRoute);
+app.use('/about', RulesRoute);
 
 app.listen(process.env.PORT, () => {
     console.log('Server is listening on port ' + process.env.PORT);
