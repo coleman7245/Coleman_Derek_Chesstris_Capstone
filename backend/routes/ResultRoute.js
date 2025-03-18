18dad7c2377d55ee1f2d08bba7f40a2246c2fc1b
coleman7245 <dlcoleman1@live.com>
2025-03-18 10:23:43 -0600

    Created result route.

---
+++ backend/routes/ResultRoute.js
@@ -0,0 +1,45 @@
+import express from 'express';
+
+import Player from '../models/Player.js';
+import Score from '../models/Score.js';
+import Time from '../modles/Time.js';
+
+const router = express.Router();
+
+router.post('/win', async (req, res) => {
+    try {
+        const player = await Player.create(req.body.player);
+        const score = await Score.create(req.body.score);
+        const time = await Time.create(req.body.time);
+        console.log(player);
+        console.log(score);
+        console.log(time);
+        res.status(200).json(player);
+        res.status(200).json(score);
+        res.status(200).json(time);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(400).json(err);
+    }
+});
+
+router.post('/lose', async (req, res) => {
+    try {
+        const player = await Player.create(req.body.player);
+        const score = await Score.create(req.body.score);
+        const time = await Time.create(req.body.time);
+        console.log(player);
+        console.log(score);
+        console.log(time);
+        res.status(200).json(player);
+        res.status(200).json(score);
+        res.status(200).json(time);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(400).json(err);
+    }
+});
+
+export default router;
