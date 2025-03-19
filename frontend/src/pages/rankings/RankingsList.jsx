be0214d58360b44c31536c1f30df0682e5e082b0
coleman7245 <dlcoleman1@live.com>
2025-03-19 16:39:57 -0600

    Modified the styling of the rankings list.

--- frontend/src/pages/rankings/RankingsList.jsx
+++ frontend/src/pages/rankings/RankingsList.jsx
@@ -2,7 +2,8 @@ import './RankingsList.css';
 
 function RankingsList({name, scores}) {
     return (
-        <ol className='rankings-list' value={name}>
+        <ol className='rankings-list'>
+            <h2>{name}</h2> <br />
             {(scores) ? scores.map((score, i) => {
                 return <><li key={i}>{`Score: ${score.player_name} - Player: ${score.score}`}</li><br /></>
                 }) : null}
