f6bdd8dc9654923969574a4349841f856ec90540
coleman7245 <dlcoleman1@live.com>
2025-08-13 17:24:36 -0600

    Fixed the timeSchema declaration and the filename for Time.js.

--- backend/models/TIme.js
+++ backend/models/TIme.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const timeSchema = mongoose.Schema({
+const timeSchema = new mongoose.Schema({
     player_name : {type : String, required : true},
     time : {type : {
         hours : {type : Number, default : 0},
